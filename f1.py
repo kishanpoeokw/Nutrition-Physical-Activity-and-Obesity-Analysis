e75446a3532f6769c263c262dd07160402a087b9
A.V.S Kishan Varma <136802308+kishanpoeokw@users.noreply.github.com>
2026-04-27 23:22:03 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
-#from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
 
